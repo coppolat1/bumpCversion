e6e169a761caea4029215f74e92ee3798ca24d32
Tom Coppola <tcoppola@naii.com>
2021-11-05 15:48:03 -0400

    Copy and use some of the utility functions from utils. Add main

--- bumpCversion.py
+++ bumpCversion.py
@@ -34,6 +34,33 @@ rPreprocessorPatch = r"""
 (?P<unsigned>[uU]?)\)   # Capture group to capture 'U' (zero or one times) and ')'
 """
 
+'''
+Utility Functions
+'''
+
+def ask_question(question):
+    answer = input('{}: '.format(question))
+    return answer.strip()
+
+
+def ask_multiple_choice_question(question, choices):
+    while True:
+        print('{}?'.format(question))
+        for i in range(len(choices)):
+            print('{}. {}'.format(i, choices[i]))
+
+        try:
+            user_choice = int(ask_question('Enter Choice'))
+        except ValueError:
+            user_choice = None
+
+        if user_choice in range(len(choices)):
+            break
+        else:
+            print('Incorrect choice. Please choose a number between 0 and {}'.format(
+                len(choices) - 1))
+    return user_choice
+
 
 def bump_revision_general(matchobj):
     """
@@ -48,19 +75,34 @@ def bump_revision_general(matchobj):
     return (str(retStr))
 
 
-print("Pre-bump string:  ", test_str)
-val = input("Bump major, minor or patch? ")
+def main():
 
-if (val == 'major'):
-    reobj = re.compile(rPreprocessorMajor, re.X)
-    rep = reobj.sub(bump_revision_general, test_str)
-elif (val == 'minor'):
-    reobj = re.compile(rPreprocessorMinor, re.X)
-    rep = reobj.sub(bump_revision_general, test_str)
-elif (val == 'patch'):
-    reobj = re.compile(rPreprocessorPatch, re.X)
-    rep = reobj.sub(bump_revision_general, test_str)
-else:
-    print('bad input')
+    print("Pre-bump string:  ", test_str)
+    #val = input("Bump major, minor or patch? ")
 
-print(rep)
+    options = ['Major',
+               'Minor',
+               'Patch',
+               'Do not update' ]
+
+    user_selected_option = ask_multiple_choice_question(
+        'Which version component would you like to bump', options)
+
+    if user_selected_option == 0:
+        reobj = re.compile(rPreprocessorMajor, re.X)
+        rep = reobj.sub(bump_revision_general, test_str)
+    elif user_selected_option == 1:
+        reobj = re.compile(rPreprocessorMinor, re.X)
+        rep = reobj.sub(bump_revision_general, test_str)
+    elif user_selected_option == 2:
+        reobj = re.compile(rPreprocessorPatch, re.X)
+        rep = reobj.sub(bump_revision_general, test_str)
+    else:
+        print('Skipping update')
+
+
+    print('Updated version to: \n', rep)
+
+
+if __name__ == '__main__':
+    main()
