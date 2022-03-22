4ff44f7872e576de673c5a706ae511a26a17b956
Travis Recupero <31936622+travisrecupero@users.noreply.github.com>
2022-03-22 15:56:54 -0400

    display versions runs independently; formatting

--- bumpCversion.py
+++ bumpCversion.py
@@ -26,48 +26,68 @@ def parse_args(argsv):
     """ Parse arguments and return them. """
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--config-file",
-        required=False,
-        help="Config file to read from. If this argument is not supplied \
-              the program will check for the existence of a configuration \
-              file with the name: \".bump.cfg\" in the current directory \
-              and use it",
-    )
-    parser.add_argument(
-        "version_file", metavar="version-file",
-        nargs='?',
-        type=extant_file,
-        help="File that contains C library version information",
-        default=None
-        #help="File to change"
-    )
-    parser.add_argument(
-        "--dry-run",
-        action='store_true',
-        help="Print out current and expected versions (without modifying files)"
-    )
-    parser.add_argument(
         "--display-versions",
-        action='store_true',
-        help="Print out all components respective versions"
-    )
-    parser.add_argument(
-        "part",
-        choices=['major', 'minor', 'patch'],
-        help="Part of the version to be bumped (major|minor|patch)"
-    )
-    parser.add_argument(
-        "--dont-reset",
-        action='store_true',
-        help=("Don't reset the patch and/or minor to zero when bumping the"
-              " major or minor versions")
-    )
-    parser.add_argument(
-        "--component",
         required=False,
-        help="A component, defined in the config file, of which to bump"
+        action='store_true'
     )
-    args = parser.parse_args(argsv)
+    parser.add_argument(
+            "--config-file",
+            required=False,
+            help="Config file to read from. If this argument is not supplied \
+                the program will check for the existence of a configuration \
+                file with the name: \".bump.cfg\" in the current directory \
+                and use it",
+    )
+    args = parser.parse_known_args()
+
+    if not args[0].display_versions:
+        parser = argparse.ArgumentParser()
+        parser.add_argument(
+            "--config-file",
+            required=False,
+            help="Config file to read from. If this argument is not supplied \
+                the program will check for the existence of a configuration \
+                file with the name: \".bump.cfg\" in the current directory \
+                and use it",
+        )
+        parser.add_argument(
+            "version_file", metavar="version-file",
+            nargs='?',
+            type=extant_file,
+            help="File that contains C library version information",
+            default=None
+            #help="File to change"
+        )
+        parser.add_argument(
+            "--dry-run",
+            action='store_true',
+            help="Print out current and expected versions (without modifying files)"
+        )
+        parser.add_argument(
+            "--display-versions",
+            action='store_true',
+            help="Print out all components respective versions"
+        )
+        parser.add_argument(
+            "part",
+            choices=['major', 'minor', 'patch'],
+            help="Part of the version to be bumped (major|minor|patch)"
+        )
+        parser.add_argument(
+            "--dont-reset",
+            action='store_true',
+            help=("Don't reset the patch and/or minor to zero when bumping the"
+                " major or minor versions")
+        )
+        parser.add_argument(
+            "--component",
+            required=False,
+            help="A component, defined in the config file, of which to bump"
+        )
+
+        args = parser.parse_args(argsv)
+    else:
+        return args[0]
 
     return args
 
@@ -122,34 +142,34 @@ def get_target_files(args):
 
     return target_files
 
-def valid_version_congruence(args, target_files, versions):
+
+def valid_version_congruence(args, versions):
     """
     Check to see if version number is the same across all files per component.
     """
     config_file_exists, cfg_components = get_config(args.config_file)
     for component in cfg_components:
         if config_file_exists:
-                while component.paths:
-                    filetype = get_filetype_object(args, component.paths)
-                    # add version to unique set
-                    versions.add(str(filetype.version_number))
-                    component.paths.pop(0)
-                if len(versions) == 1:
-                    versions.clear()
-                    continue
-                else:
-                    print("Multiple Verisons Found -> " + str(versions))
-                    raise(VersionException(Exception))
-            
+            while component.paths:
+                filetype = get_filetype_object(args, component.paths)
+                # add version to unique set
+                versions.add(str(filetype.version_number))
+                component.paths.pop(0)
+            if len(versions) == 1:
+                versions.clear()
+                continue
+            else:
+                print("Multiple Verisons Found -> " + str(versions))
+                raise(VersionException(Exception))
+
     return 1
-    
 
 
 def print_dry(args, target_files, versions):
     """
     Print expected bump value of version found from first target file.
     """
-    if valid_version_congruence(args, target_files.copy(), versions):
+    if valid_version_congruence(args, versions):
         if target_files:
             filetype = get_filetype_object(args, target_files)
             print("Current version = " + str(filetype.version_number))
@@ -158,17 +178,16 @@ def print_dry(args, target_files, versions):
                   str(filetype.version_number) + "\n")
 
 
-def print_versions(args, target_files, versions):
+def print_versions(args, versions):
     """
     Print expected bump value of version found from first target file.
     """
-    if valid_version_congruence(args, target_files.copy(), versions):
+    if valid_version_congruence(args, versions):
         config_file_exists, cfg_components = get_config(args.config_file)
         for component in cfg_components:
-            print("Component -> [" + component.name + "]")
             if config_file_exists:
                 filetype = get_filetype_object(args, component.paths)
-                print("Current version = " + str(filetype.version_number))
+                print("Component: [" + component.name + "]" + " -> "+ str(filetype.version_number))
 
 
 def get_filetype_object(args, target_files):
@@ -192,29 +211,32 @@ def main(argsv=None):
     # Parse command line arguments
     args = parse_args(argsv)
 
+    # Create list of found versions (length should be == 1)
+    versions = set()
+
+    # Check for display versions
+    if args.display_versions:
+        if args.config_file:
+            print_versions(args, versions)
+        else:
+            print('Please specify config file.')
+        exit()
+    
     # Users desired part (major, minor, or patch) to bump
     part_to_bump = args.part
 
     # Get file we're interested in
     target_files = get_target_files(args)
 
-    # Create list of found versions (length should be == 1)
-    versions = set()
-
-    # Check for display versions
-    if args.display_versions:
-        print_versions(args, target_files.copy(), versions)
-        exit()
-
     # Check for dry run
     if args.dry_run:
-        print_dry(args, target_files.copy(), versions)
+        print_dry(args, versions)
         exit()
 
     # Check to see if version number is the same across all files
     print("Checking component [" + str(args.component) +
           "] for verison number congruence...\n")
-    valid_version_congruence(args, target_files.copy(), versions)
+    valid_version_congruence(args, versions)
 
     while target_files:
 
--- tests/test_version.py
+++ tests/test_version.py
@@ -1,6 +1,10 @@
 import pytest
-from bumpCversion import main
+from bumpCversion import main, parse_args
 
+
+@pytest.fixture
+def my_parser():
+    return parse_args("--dry-run")
 
 @pytest.mark.parametrize("option", ("-h", "--help"))
 def test_help(capsys, option):
@@ -13,8 +17,10 @@ def test_help(capsys, option):
 
 
 # @pytest.mark.parametrize("dryrun", (None, "--dry-run"))
-# def test_dry(capsys, dryrun):
+# def test_arg(capsys, dryrun):
 #     print_dry()
 #     output = capsys.readouterr()
 #     assert "Current version =" in output
 
+# def test_something():
+#         assert "Current" in my_parser
