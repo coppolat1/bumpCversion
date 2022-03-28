92bc6e22b44b3be54199b045afa0deaf2aaf17f6
Travis Recupero <31936622+travisrecupero@users.noreply.github.com>
2022-03-28 19:40:53 -0400

    added test cases and imported magic

--- tests/test_version.py
+++ tests/test_version.py
@@ -2,20 +2,36 @@ import pytest
 from typer.testing import CliRunner
 from bumpCversion import app
 from filetypes import SemanticVersionNumber
-# import magic
+import magic
 
 runner = CliRunner()
 
-@pytest.fixture(name='doxydir')
-def doxydir():
-    with open('./tests/test_version/Doxyfile', 'r', errors="ignore",encoding='utf-8', newline= '') as input:
+
+@pytest.fixture(name='doxy_contents')
+def doxy_contents():
+    with open('./tests/test_version/Doxyfile', 'r', errors="ignore", encoding='utf-8', newline='') as input:
         content = input.read()
     return content
 
-# @pytest.fixture(name='doxyfile')
-# def doxyfile():
-#     ret_val = magic.from_file('Doxyfile')
-#     return ret_val
+
+@pytest.fixture(name='doxyfile')
+def doxyfile():
+    ret_val = magic.from_file('./tests/test_version/Doxyfile')
+    return ret_val
+
+
+@pytest.fixture(name='header_contents')
+def header_contents():
+    with open('./tests/test_version/sample-input-file.h', 'r', errors="ignore", encoding='utf-8', newline='') as input:
+        content = input.read()
+    return content
+
+
+@pytest.fixture(name='header_file')
+def header_file():
+    ret_val = magic.from_file('./tests/test_version/sample-input-file.h')
+    return ret_val
+
 
 @pytest.fixture(name='version_num')
 def version_num():
@@ -23,35 +39,58 @@ def version_num():
     return version
 
 
-def test_dry():
+def test_dry_output():
     result = runner.invoke(
         app, ["dry-run", "./sample_files/.bump.cfg", "naibsp", "minor"])
     assert result.exit_code == 0
     assert "Current version" in result.stdout
 
-# def test_doxy_type(doxyfile):
-#     x = 0
-#     assert x == 0
 
-def test_doxy_vaild(doxydir):
-    assert 'PROJECT_NAME' in doxydir
+def test_display_version_output():
+    result = runner.invoke(
+        app, ["display-version", "./sample_files/.bump.cfg"])
+    assert result.exit_code == 0
+    assert "Component:" in result.stdout
+
+
+def test_doxy_type(doxyfile):
+    magic_doxy = 'ASCII text, with CRLF line terminators'
+    assert doxyfile == magic_doxy
+
+
+def test_doxy_valid(doxy_contents):
+    assert 'PROJECT_NAME' in doxy_contents
+
+
+def test_header_type(header_file):
+    header = 'C source, ASCII text, with CRLF line terminators'
+    assert header_file == header
+
+
+def test_header_valid(header_contents):
+    assert '#ifndef' or '#IFNDEF' in header_contents
+
 
 def test_version(version_num):
     assert version_num.major == 1
     assert version_num.minor == 2
     assert version_num.patch == 3
 
+
 def test_bump_major(version_num):
     version_num.bump("major", False)
     assert version_num.major == 2
+
 
 def test_bump_minor(version_num):
     version_num.bump("minor", False)
     assert version_num.minor == 3
 
+
 def test_bump_patch(version_num):
     version_num.bump("patch", False)
     assert version_num.patch == 4
+
 
 def test_bump_major_reset(version_num):
     version_num.bump("major", True)
@@ -59,14 +98,13 @@ def test_bump_major_reset(version_num):
     assert version_num.minor == 0
     assert version_num.patch == 0
 
+
 def test_bump_minor_reset(version_num):
     version_num.bump("minor", True)
     assert version_num.minor == 3
     assert version_num.patch == 0
 
+
 def test_bump_patch_reset(version_num):
     version_num.bump("patch", True)
     assert version_num.patch == 4
-    
-
-
